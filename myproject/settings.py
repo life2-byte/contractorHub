706b8bcfbb79cbc2e62d12788a4fc6d94e492d7d
life2-byte <achancelifeis7@gmail.com>
2026-06-01 16:34:00 +0500

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['contractorhub-4.onrender.com']
 
 AUTH_USER_MODEL = 'myapp.signin'
 # Application definition
