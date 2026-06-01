2e22a4b56b0a98d5fcdddf41754da5a8b3b391fc
life2-byte <achancelifeis7@gmail.com>
2026-06-01 17:02:38 +0500

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -168,7 +168,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'  # slash add karo dono sides
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL  = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
